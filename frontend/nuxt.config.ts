358bbf24fb068eb099853863bb04d3a95d666089
PANTE Wills <pantewills1@gmail.com>
2026-05-17 19:54:27 +0100

    feat(VTOG-1): initialisation de l'architecture frontend et postgresql

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,50 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // 1. Les modules d'abord (Recommandation ESLint)
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vite-pwa/nuxt',
+    '@pinia/nuxt'
+  ],
+
+  // 2. Les configurations globales
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  // 3. La configuration des modules spécifiques
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'DPGlassess AR',
+      short_name: 'DPGlassess',
+      theme_color: '#10B981',
+      icons: [
+        { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+        { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    }
+  }
+})
